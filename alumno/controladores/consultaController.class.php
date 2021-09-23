d4a3aabdf390065e79d1e5c561863cb6466ae255
pedrooyarzun-uy <pedrooyarzun2015@gmail.com>
2021-09-23 17:38:23 -0300

    Van controladores y modelos

---
+++ alumno/controladores/consultaController.class.php
@@ -0,0 +1,65 @@
+<?php  
+    require '../utils/autoloader.php';
+
+    class consultaController {
+        public static function preAltaDeConsulta($mensajeConsulta, $cedulaProfesor){
+            if( $mensajeConsulta != "" && $cedulaProfesor != "" ){
+                try{
+                    $p = new ConsultaModelo();
+                    $p -> mensajeConsulta = $mensajeConsulta;
+                    $p -> cedulaAlumno = $_SESSION['cedula'];
+                    $p -> cedulaProfesor = $cedulaProfesor;
+                    $p -> estadoConsulta ="enviado";
+                    $p -> usuarioAlumno = $_SESSION['usuario'];
+                    $p -> guardarConsulta();
+
+                    return generarHtml('consultaAlumno', ['exito' => true]);
+                }
+                catch(Exception $e){
+                    error_log($e -> getMessage());
+                    return "No se pudo guardar la consulta";
+                }
+            }else{
+                return generarHtml('consultaAlumno',  ['exito' => false]);
+            }
+        }
+
+
+        public static function mostrarConsultas(){
+            $a = new ConsultaModelo();
+            $consultasEnviadas = $a -> listarConsultas();
+            return $consultasEnviadas;
+
+        }
+
+
+        public static function insertarRespuesta( $mensajeRespuesta, $idConsulta ){
+           
+            if( $mensajeRespuesta != "" &&  $idConsulta !=""){
+                try{
+                    $p = new ConsultaModelo();
+                    $p -> idConsulta= $idConsulta;
+                    $p -> mensajeRespuesta = $mensajeRespuesta;
+                    $p -> usuarioProfesor = $_SESSION['usuario'];
+                    $p -> guardarRespuesta();
+                    return generarHtml('responderConsultas', ['exito' => true]);
+                }
+                catch(Exception $e){
+                    error_log($e -> getMessage());
+                    return "No se pudo guardar la respeusta";
+                }
+            }else{
+                return generarHtml('responderConsultas',  ['exito' => false]); 
+            }
+        }
+
+        public static function mostrarRespuesta(){
+            $a = new ConsultaModelo();
+            $respuestasEnviadas = $a -> listarRespuesta();
+            return $respuestasEnviadas;
+
+        }
+
+
+
+    }
---
+++ alumno/modelos/consultaModelo.class.php
@@ -0,0 +1,84 @@
+<?php
+
+require '../utils/autoloader.php';
+
+class consultaModelo extends Modelo{
+    public $idConsulta;
+    public $mensajeConsulta; 
+    public $mensajeRespuesta; 
+    public $cedulaAlumno;
+    public $cedulaProfesor;
+    public $estadoConsulta;
+    public $usuarioAlumno;  
+    public $usuarioProfesor;
+
+
+    private function prepararInsercionDeConsulta(){   
+            $sql = "INSERT into consulta (mensajeConsulta, mensajeRespuesta, cedulaAlumno, cedulaProfesor, estadoConsulta, usuarioAlumno, usuarioProfesor) VALUES (?,?,?,?,?,?,?)";
+            $this -> sentencia = $this -> conexion -> prepare($sql);
+            $this -> sentencia -> bind_param("ssiisss",
+                $this -> mensajeConsulta,
+                $this -> mensajeRespuesta,
+                $this -> cedulaAlumno,
+                $this -> cedulaProfesor,
+                $this -> estadoConsulta,
+                $this -> usuarioAlumno, 
+                $this -> usuarioProfesor,
+                ); 
+    }
+
+
+    private function prepararListadoDeConsultas(){
+        $sql = "SELECT idConsulta, mensajeConsulta, mensajeRespuesta, cedulaAlumno, cedulaProfesor, estadoConsulta, usuarioAlumno, usuarioProfesor FROM consulta WHERE cedulaProfesor= ? && estadoConsulta='enviado'" ;
+        $this -> sentencia = $this -> conexion -> prepare($sql);
+        $this -> sentencia -> bind_param("i", $_SESSION['cedula']);
+        $this -> sentencia -> execute();
+    }
+
+
+    public function listarConsultas(){
+        $this -> prepararListadoDeConsultas();
+        $this -> sentencia -> execute();
+        $resultado = $this -> sentencia -> get_result() -> fetch_all(MYSQLI_ASSOC);   
+        return $resultado;
+    }
+   
+
+    public function guardarConsulta(){
+        $this -> prepararInsercionDeConsulta(); 
+        $this -> sentencia -> execute();   
+         
+    }
+    public function guardarRespuesta(){
+        $this -> prepararInsercionDeRespuesta(); 
+        $this -> sentencia -> execute();   
+         
+    }
+    private function prepararInsercionDeRespuesta(){
+        $sql = "UPDATE consulta SET mensajeRespuesta= ?, usuarioProfesor= ?, estadoConsulta='respondido' WHERE idConsulta= ? ";
+        $this -> sentencia = $this -> conexion -> prepare($sql);
+        $this -> sentencia -> bind_param("ssi",
+            $this -> mensajeRespuesta,
+            $this -> usuarioProfesor,
+            $this -> idConsulta,
+            ); 
+        //$this ->  sentencia -> execute();
+    }
+
+
+    private function prepararListadoDeRespuesta(){    
+        $sql = "SELECT idConsulta, mensajeConsulta, mensajeRespuesta, cedulaAlumno, cedulaProfesor, estadoConsulta, usuarioAlumno, usuarioProfesor FROM consulta WHERE cedulaAlumno= ? && estadoConsulta='respondido'" ;
+        $this -> sentencia = $this -> conexion -> prepare($sql);
+        $this -> sentencia -> bind_param("i", $_SESSION['cedula']);
+    }
+
+
+    public function listarRespuesta(){
+        $this -> prepararListadoDeRespuesta();
+        $this -> sentencia -> execute();
+        $resultado = $this -> sentencia -> get_result() -> fetch_all(MYSQLI_ASSOC);   
+        return $resultado;
+    }
+
+
+}
